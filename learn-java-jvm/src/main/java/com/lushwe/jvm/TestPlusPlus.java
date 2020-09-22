a9aa8cb669a0fd45d69e038d1c74695d3c6e8a23
Jack Liu <947613622@qq.com>
2020-09-22 10:27:47 +0800

    测试i++ ++i字节码指令

---
+++ learn-java-jvm/src/main/java/com/lushwe/jvm/TestPlusPlus.java
@@ -0,0 +1,21 @@
+package com.lushwe.jvm;
+
+/**
+ * 说明：测试++
+ *
+ * @author Jack Liu
+ * @date 2020-09-21 21:06
+ * @since 0.1
+ */
+public class TestPlusPlus {
+
+    public static void main(String[] args) {
+
+        int i = 8;
+        i = i++;
+//        i = ++i;
+
+        System.out.println(i);
+
+    }
+}
