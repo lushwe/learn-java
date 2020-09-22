891aa87be198863059e34ace47fce0e43f0d878b
Jack Liu <947613622@qq.com>
2020-09-22 10:43:47 +0800

    查看main方法字节码指令

---
+++ learn-java-jvm/src/main/java/com/lushwe/jvm/bytecode/ByteCode01.java
@@ -0,0 +1,15 @@
+package com.lushwe.jvm.bytecode;
+
+/**
+ * 说明：TODO 写点注释吧
+ *
+ * @author Jack Liu
+ * @date 2020-09-22 10:32
+ * @since 0.1
+ */
+public class ByteCode01 {
+
+    public static void main(String[] args) {
+
+    }
+}
