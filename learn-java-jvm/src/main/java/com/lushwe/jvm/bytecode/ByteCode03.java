7ac7ef10b5128773a71608ba3b7721e67d6e7fbb
Jack Liu <947613622@qq.com>
2020-09-22 10:44:54 +0800

    查看静态方法字节码指令

---
+++ learn-java-jvm/src/main/java/com/lushwe/jvm/bytecode/ByteCode03.java
@@ -0,0 +1,19 @@
+package com.lushwe.jvm.bytecode;
+
+/**
+ * 说明：查看静态方法字节码指令
+ *
+ * @author Jack Liu
+ * @date 2020-09-22 10:32
+ * @since 0.1
+ */
+public class ByteCode03 {
+
+    public static void main(String[] args) {
+        run(1);
+    }
+
+    public static void run(int i) {
+
+    }
+}
