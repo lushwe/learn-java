b8cbead37d5005a71c270a08a5e480ef9485fc24
Jack Liu <947613622@qq.com>
2020-09-22 10:44:26 +0800

    查看成员变量字节码指令

---
+++ learn-java-jvm/src/main/java/com/lushwe/jvm/bytecode/ByteCode02.java
@@ -0,0 +1,17 @@
+package com.lushwe.jvm.bytecode;
+
+/**
+ * 说明：查看成员变量字节码指令
+ *
+ * @author Jack Liu
+ * @date 2020-09-22 10:32
+ * @since 0.1
+ */
+public class ByteCode02 {
+
+    private int i = 9;
+
+    public static void main(String[] args) {
+
+    }
+}
