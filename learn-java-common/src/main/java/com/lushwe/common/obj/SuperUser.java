9fd741c50fcdd838d73214e1d02a99c596c482f8
Jack Liu <947613622@qq.com>
2020-10-04 22:34:59 +0800

    SuperUser对象

---
+++ learn-java-common/src/main/java/com/lushwe/common/obj/SuperUser.java
@@ -0,0 +1,16 @@
+package com.lushwe.common.obj;
+
+import lombok.Data;
+
+/**
+ * 说明：超类
+ *
+ * @author Jack Liu
+ * @date 2020-08-27 15:13
+ * @since 0.1
+ */
+@Data
+public class SuperUser {
+
+    private String superAddr;
+}
