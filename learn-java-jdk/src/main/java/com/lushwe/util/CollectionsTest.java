947e32f1c8845ee3fba6b1cec91222a27cbe4d73
Jack Liu <947613622@qq.com>
2020-11-04 17:42:36 +0800

    Collections工具类-反转列表

---
+++ learn-java-jdk/src/main/java/com/lushwe/util/CollectionsTest.java
@@ -0,0 +1,42 @@
+package com.lushwe.util;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 说明：Collections使用测试
+ *
+ * @author Jack Liu
+ * @date 2020-08-04 15:24
+ * @since 0.1
+ */
+public class CollectionsTest {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        System.out.println("测试 reverse");
+        reverse(list);
+
+    }
+
+    /**
+     * 反转列表
+     *
+     * @param list
+     */
+    public static void reverse(List<String> list) {
+        System.out.println(JSON.toJSONString(list, true));
+        Collections.reverse(list);
+        System.out.println(JSON.toJSONString(list, true));
+    }
+}
